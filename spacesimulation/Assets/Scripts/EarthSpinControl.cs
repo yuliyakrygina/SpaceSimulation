de3b4e588862984eb81ce7a51df78c1ccf4b2218
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-10-22 21:24:08 -0400

    Added and took away assets

---
+++ spacesimulation/Assets/Scripts/EarthSpinControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	//continue 
+	void Update () {
+
+        transform.Rotate(0, 0.5f, 0);
+	}
+}
