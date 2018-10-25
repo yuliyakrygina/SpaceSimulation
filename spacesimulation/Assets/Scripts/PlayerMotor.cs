424711fbfab5b3d57d434827e813689ee6ef8928
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-10-25 10:15:36 -0400

    Axis Tilts on planets

--- spacesimulation/Assets/Scripts/PlayerMotor.cs
+++ spacesimulation/Assets/Scripts/PlayerMotor.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour {
 
+    //Putting PlayerController in action. 
+
     public Camera cam;
 
     private Vector3 velocity = Vector3.zero;
@@ -25,34 +27,32 @@ public class PlayerMotor : MonoBehaviour {
         velocity = _velocity;
     }
 
-    public void Rotate(Vector3 _rotation)
+    public void Rotate (Vector3 _rotation)
     {
         rotation = _rotation;
-
     }
 
-    public void RotateCamera(Vector3 _cameraRotation)
+    public void RotateCamera (Vector3 _cameraRotation)
     {
         cameraRotation = _cameraRotation;
-
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() //physics 
     {
         PerformMovement();
         PerformRotation();
     }
 
-    void PerformMovement()
+    void PerformMovement() //already performing as FixedUpdate
     {
         if(velocity != Vector3.zero)
         {
-            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime); //Time. Interface to get time information
         }
 
     }
 
-    void PerformRotation()
+    void PerformRotation() //already performing as FixedUpdate
     {
         rb.MoveRotation(rb.rotation * Quaternion.Euler (rotation));
         if(cam != null)
