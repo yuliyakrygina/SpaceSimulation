f352d18cacabb23acf567b3e97ca9dcc194ee396
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-10-25 16:46:11 -0400

    changing playercontroller script

--- spacesimulation/Assets/Scripts/PlayerController.cs
+++ spacesimulation/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+[RequireComponent(typeof(PlayerMotor))]
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,33 +11,58 @@ public class PlayerController : MonoBehaviour {
     public float LookSensitivity; 
 
     private PlayerMotor motor;
+    private Rigidbody rb;
+    public float velocity;
 
     void Start()
     {
-        motor = GetComponent<PlayerMotor>(); 
+        motor = GetComponent<PlayerMotor>(); //fetch playmotor from game object
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        //player movement
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float zMove = Input.GetAxisRaw("Vertical");
 
-        Vector3 MoveHorizontal = transform.forward * xMove;
-        Vector3 MoveVertical = transform.right * zMove;
-        Vector3 _velocity = (MoveHorizontal + MoveVertical).normalized * speed; 
-        motor.Move(_velocity); //Move, defined in PlayerMotor
+       
+        if (Input.GetKey(KeyCode.UpArrow)){ //doesnt need vector3 because it is just going up or down
 
+            rb.velocity = transform.up * speed * Time.fixedDeltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+
+            rb.velocity = -transform.up * speed * Time.fixedDeltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) //vector3 needed because it is rotating
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * velocity * Time.fixedDeltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -1) * velocity * Time.fixedDeltaTime, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.Space)) //figure out 
+        {
+
+            rb.velocity = new Vector3(-1, 0, 0) * speed * Time.fixedDeltaTime;
+        }
+       
+
+
+
+        
         //playerrotation
-        float yRot = Input.GetAxisRaw("Mouse X");
-        Vector3 _rotation = new Vector3(0f, yRot, 0f) * LookSensitivity; 
+        float yRot = Input.GetAxisRaw("Mouse X"); 
+        Vector3 _rotation = new Vector3(0f, yRot, 0f) * LookSensitivity;  
         motor.Rotate(_rotation);
-
-
+        
+        
         //camerarotation 
         float xRot = Input.GetAxisRaw("Mouse Y");
         Vector3 _cameraRotation = new Vector3(xRot, 0f, 0f) * LookSensitivity;
         motor.RotateCamera(_cameraRotation);
-
+        
     }
 }
