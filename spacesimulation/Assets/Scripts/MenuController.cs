4152c929d289b5d553d23c5273f9fa477cfd9ee8
Yuliya Krygina <36555259+yuliyakrygina@users.noreply.github.com>
2018-11-28 13:51:04 -0500

    ----

---
+++ spacesimulation/Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartSim() {
+
+        SceneManager.LoadScene("New Scene"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
